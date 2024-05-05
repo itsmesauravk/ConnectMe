05b504fb7b3109520d50708385549e0aaeae0443
itsmesauravk <sauravkarki10.12@gmail.com>
2024-05-05 23:44:54 +0545

    updated

--- client/src/Layout.js
+++ client/src/Layout.js
@@ -16,13 +16,13 @@ const YourComponent = ({ mode, toggleMode }) => {
     <div className="flex justify-between pr-3 pl-3 items-center">
       <h1 className="connect font-bold">Connect<span className="us">Us</span></h1>
       <div className="flex gap-2">
-        <p>{currentTime.toLocaleTimeString()}</p>
+        {/* <p>{currentTime.toLocaleTimeString()}</p>
         
         <label className="relative inline-flex items-center cursor-pointer">
           <input type="checkbox" value="" className="sr-only peer" onClick={()=>toggleMode()}/>
           <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
           <span className={`ms-3 text-sm font-medium ${mode === 'light' ? 'dark-text' : 'light-text'}`}>{mode === 'light' ? 'Dark' : 'Light'}</span>
-        </label>
+        </label> */}
 
       </div>
     </div>
